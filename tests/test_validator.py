d1229fd1fd2ac4f99aa30cd2521dd51ecd08b6d6
Breno Jesus Fernandes <fernandes.breno@pm.me>
2023-01-27 22:14:25 -0300

    Style: format scripts with blue

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,6 +1,7 @@
 from cronus_eater import _validator
 
+
 def test_is_magic():
-    assert  _validator.is_magic(1)
+    assert _validator.is_magic(1)
     assert not _validator.is_magic(0)
     assert not _validator.is_magic(-1)
