8c2ae3659cc0bc04f8e739ec82d3d3e3c55842b1
Breno Jesus Fernandes <fernandes.breno@pm.me>
2023-02-02 22:01:44 -0300

    FEAT: Add verification financial number

--- cronus_eater/_validator.py
+++ cronus_eater/_validator.py
@@ -1,3 +1,4 @@
+import re
 from typing import Any
 
 import numpy as np
@@ -13,6 +14,61 @@ def is_blank_value(value: Any) -> bool:
         return True
 
     if str(value).strip().lower() in ('-', 'none', 'null', 'nan'):
+        return True
+
+    return False
+
+
+def __is_normal_float(value: str) -> bool:
+    if re.match(r'[$]?[\s]?[-]?[\d]+(([.]|[,])[\d]+)?$', value):
+        return True
+
+    return False
+
+
+def __is_float_with_comma_sep(value: str) -> bool:
+    if re.match(r'[$]?[\s]?[-]?[\d]{1,3}([,][\d]{3})*([.][\d]+)?$', value):
+        return True
+
+    return False
+
+
+def __is_float_with_dot_sep(value: str) -> bool:
+    if re.match(r'[$]?[\s]?[-]?[\d]{1,3}([.][\d]{3})*([,][\d]+)?$', value):
+        return True
+
+    return False
+
+
+def __is_float_with_space_sep(value: str) -> bool:
+    if re.match(
+        r'[$]?[\s]?[-]?[\d]{1,3}([\s][\d]{3})*(([.]|[,])[\d]+)?$', value
+    ):
+        return True
+
+    return False
+
+
+def __is_percent(value: str) -> bool:
+    if re.match(r'[-]?[\d]+(([.]|[,])[\d]+)?[\s]?[%]$', value):
+        return True
+
+    return False
+
+
+def is_financial_number(value: Any) -> bool:
+    if is_blank_value(value):
+        return False
+
+    text = str(value).strip()
+
+    if (
+        __is_normal_float(text)
+        or __is_float_with_dot_sep(text)
+        or __is_float_with_comma_sep(text)
+        or __is_float_with_space_sep(text)
+        or __is_percent(text)
+    ):
         return True
 
     return False
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -18,3 +18,42 @@ def test_is_blank_value():
     assert _validator.is_blank_value('   -    ')
     assert _validator.is_blank_value('  NaN   ')
     assert _validator.is_blank_value('  Null  ')
+
+
+def test_is_financial_number():
+    assert _validator.is_financial_number('0')
+    assert _validator.is_financial_number('-1023213')
+    assert _validator.is_financial_number('1023213')
+    assert _validator.is_financial_number('$ -1023213')
+    assert _validator.is_financial_number('$ 1023213')
+
+    assert _validator.is_financial_number('0.0')
+    assert _validator.is_financial_number('0,0')
+    assert _validator.is_financial_number('-10000231.132123')
+    assert _validator.is_financial_number('-10000231,132123')
+    assert _validator.is_financial_number('10000231.132123')
+    assert _validator.is_financial_number('10000231,132123')
+    assert _validator.is_financial_number('$ 10000231.132123')
+    assert _validator.is_financial_number('$ 10000231,132123')
+
+    assert _validator.is_financial_number('10,000,231')
+    assert _validator.is_financial_number('-10,000,231')
+    assert _validator.is_financial_number('10,000,231.24')
+    assert _validator.is_financial_number('-10,000,231.24')
+    assert _validator.is_financial_number('$ 10,000,231.24')
+    assert _validator.is_financial_number('$ -10,000,231.24')
+
+    assert _validator.is_financial_number('10.000.231')
+    assert _validator.is_financial_number('-10.000.231')
+    assert _validator.is_financial_number('10.000.231,24')
+    assert _validator.is_financial_number('-10.000.231,24')
+    assert _validator.is_financial_number('$ 10.000.231,24')
+    assert _validator.is_financial_number('$ -10.000.231,24')
+
+    assert _validator.is_financial_number('17.37 %')
+    assert _validator.is_financial_number('-1.1 %')
+    assert _validator.is_financial_number('-1,1 %')
+    assert _validator.is_financial_number('1,1 %')
+
+    assert not _validator.is_financial_number('V1.2')
+    assert not _validator.is_financial_number('1.234.23')
