a2e6cff7d012daaee0f651b4629b3eaee529ac50
Breno Jesus Fernandes <fernandes.breno@pm.me>
2023-02-02 21:31:20 -0300

    FEAT: Detect if a cell has a real not blank value

--- cronus_eater/_validator.py
+++ cronus_eater/_validator.py
@@ -1,2 +1,18 @@
-def is_magic(value: int) -> bool:
-    return value > 0
+from typing import Any
+
+import numpy as np
+import pandas as pd
+
+
+def is_blank_value(value: Any) -> bool:
+
+    if pd.isnull(value):
+        return True
+
+    if len(str(value).strip()) == 0:
+        return True
+
+    if str(value).strip().lower() in ('-', 'none', 'null', 'nan'):
+        return True
+
+    return False
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,7 +1,20 @@
+from datetime import datetime
+
+import numpy as np
+
 from cronus_eater import _validator
 
 
-def test_is_magic():
-    assert _validator.is_magic(1)
-    assert not _validator.is_magic(0)
-    assert not _validator.is_magic(-1)
+def test_is_blank_value():
+    assert _validator.is_blank_value(np.nan)
+    assert _validator.is_blank_value(None)
+
+    assert not _validator.is_blank_value({})
+    assert not _validator.is_blank_value(())
+    assert not _validator.is_blank_value(datetime.now())
+    assert not _validator.is_blank_value(object)
+
+    assert _validator.is_blank_value('        ')
+    assert _validator.is_blank_value('   -    ')
+    assert _validator.is_blank_value('  NaN   ')
+    assert _validator.is_blank_value('  Null  ')
