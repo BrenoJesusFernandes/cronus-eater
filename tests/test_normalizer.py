5e3a88709eeb845fa24b1dff5b9d3039a88c3d9c
Breno Jesus Fernandes <fernandes.breno@pm.me>
2023-02-06 19:14:34 -0300

    FIX: tests norm_blank_value should validate with pd.NA value

--- tests/test_normalizer.py
+++ tests/test_normalizer.py
@@ -1,13 +1,14 @@
 import numpy as np
+import pandas as pd
 
 from cronus_eater import _normalizer
 
 
 def test_norm_blank_value():
-    assert _normalizer.norm_blank_value(np.nan) is np.nan
-    assert _normalizer.norm_blank_value(None) is np.nan
+    assert _normalizer.norm_blank_value(np.nan) is pd.NA
+    assert _normalizer.norm_blank_value(None) is pd.NA
 
-    assert _normalizer.norm_blank_value('') is np.nan
-    assert _normalizer.norm_blank_value('    ') is np.nan
-    assert _normalizer.norm_blank_value(' -   ') is np.nan
-    assert _normalizer.norm_blank_value('NaN  ') is np.nan
+    assert _normalizer.norm_blank_value('') is pd.NA
+    assert _normalizer.norm_blank_value('    ') is pd.NA
+    assert _normalizer.norm_blank_value(' -   ') is pd.NA
+    assert _normalizer.norm_blank_value('NaN  ') is pd.NA
