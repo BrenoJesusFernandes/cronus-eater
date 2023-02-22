0117e8f7f5affcc82eee3e318d8b9db6b68bc4a2
breno-jesus-fernandes <breno.de.jesus.fernandes@gmail.com>
2023-02-22 00:56:04 -0300

    FIX: project config to publish

--- cronus_eater/__init__.py
+++ cronus_eater/__init__.py
@@ -1,4 +1,4 @@
 from cronus_eater._extractor import find_time_series
 
-__author__ = 'Breno Fernandes <brenojesusfernandes@pm.com>'
-__version__ = '0.1.0'
+__author__ = 'Breno Fernandes <breno.de.jesus.fernandes@gmail.com>'
+__version__ = '0.1.0a1'
