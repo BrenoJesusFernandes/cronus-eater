1336773babf49a41ebe0acede582e67f46461571
breno-jesus-fernandes <breno.de.jesus.fernandes@gmail.com>
2024-02-01 00:30:26 -0300

    Style: fix main file style

--- cronus_eater/__main__.py
+++ cronus_eater/__main__.py
@@ -6,16 +6,16 @@ from cronus_eater import _converter
 
 
 def main():
-    logger.info("Reading File ...")
+    logger.info('Reading File ...')
     raw_dataframes = pd.read_excel(
-        "tests/integration/data/nubank_3Q22.xlsx", header=None, sheet_name=None
+        'tests/integration/data/nubank_3Q22.xlsx', header=None, sheet_name=None
     )
 
-    logger.info("Processing Time Series ...")
-    times_series_df = cronus_eater.extract_many(raw_dataframes, mode="tidy")
+    logger.info('Processing Time Series ...')
+    times_series_df = cronus_eater.extract_many(raw_dataframes, mode='tidy')
 
     logger.info(times_series_df)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
